aa75a41919f2a8191620b3f1e0c3dc5a5c0cd762
luis <xsub-zerox06@hotmail.com>
2023-06-09 18:16:09 -0400

    Add project files.

---
+++ NewRecycle/newrecycle.aspx
@@ -0,0 +1,167 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="newrecycle.aspx.cs" Inherits="NewRecycle.WebForm1" %>
+<!DOCTYPE html>
+<html>
+
+<head runat="server">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Recycle IIS Web Applications</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 20px;
+            text-align: center;
+        }
+
+        h1 {
+            margin-bottom: 20px;
+            color: #333;
+        }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            max-width: 400px;
+            margin: 0 auto;
+            background-color: #f2f2f2;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+        }
+
+        .container label {
+            margin-bottom: 10px;
+            font-weight: bold;
+            color: #333;
+        }
+
+        .container select {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+            font-size: 16px;
+        }
+
+        .container button {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        #message {
+            margin-top: 20px;
+            font-weight: bold;
+            color: #333;
+        }
+
+        .animation-container {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: rgba(0, 0, 0, 0.5);
+            z-index: 999;
+            opacity: 0;
+            transition: opacity 0.3s ease-in-out;
+            pointer-events: none;
+        }
+
+        .animation-text {
+            color: white;
+            font-size: 24px;
+        }
+
+        .animation-fade-in {
+            opacity: 1;
+        }
+
+        .animation-fade-out {
+            opacity: 0;
+        }
+
+        .customer-names {
+        font-size: 20px;
+        font-weight: bold;
+        }
+
+        .logo {
+            display: flex;
+            justify-content: center;
+            margin-bottom: 20px;
+        }
+        
+        .logo img {
+            max-width: 10%;
+            height: auto;
+        }
+
+        @media screen and (max-width: 480px) {
+            /* Mobile styles */
+            .container {
+                max-width: 100%;
+                padding: 10px;
+            }
+
+            .container select {
+                font-size: 14px;
+            }
+
+            .container button {
+                padding: 8px 16px;
+                font-size: 14px;
+            }
+        }
+    </style>
+
+    <script>
+        function showAnimation() {
+            var animationContainer = document.getElementById('animationContainer');
+            animationContainer.classList.add('animation-fade-in');
+            animationContainer.classList.remove('animation-fade-out');
+
+            setTimeout(function () {
+                hideAnimation();
+            }, 2000); // Hide the animation after 2 seconds
+        }
+
+        function hideAnimation() {
+            var animationContainer = document.getElementById('animationContainer');
+            animationContainer.classList.remove('animation-fade-in');
+            animationContainer.classList.add('animation-fade-out');
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <div class="logo">
+            <img src="IIS_logo.png" alt="IIS Logo" />
+        </div>
+
+        <h1>Recycle IIS Web Applications</h1>
+        
+        <div class="container">
+            <asp:Label runat="server" AssociatedControlID="webAppList">Select an IIS Web Application:</asp:Label>
+            <div class="animation-container" id="animationContainer">
+                <div class="animation-text">Processing...</div>
+            </div>
+            <asp:DropDownList ID="webAppList" runat="server" CssClass="form-control"></asp:DropDownList>
+            <asp:Button ID="recycleButton" runat="server" Text="Recycle" OnClick="RecycleButton_Click" CssClass="btn-primary" OnClientClick="showAnimation();" />
+            <div id="message" runat="server"></div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ NewRecycle/newrecycle.aspx.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.Web.Administration;
+
+
+
+namespace NewRecycle
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Load the XML file and populate the dropdown list
+                LoadWebApps();
+            }
+
+            // Apply bigger font and bold style to customer names in the dropdown handled by <Customer> tags in XML
+            foreach (ListItem item in webAppList.Items)
+            {
+                if (IsCustomerName(item.Text))
+                {
+                    item.Attributes["style"] = "font-size: 20px; font-weight: bold;";
+                }
+            }
+
+            // Set the CSS class for styling the customer names
+            webAppList.CssClass = "customer-names";
+
+        }
+
+        protected void RecycleButton_Click(object sender, EventArgs e)
+        {
+
+            string selectedWebApp = webAppList.SelectedItem.Text;
+            // Log IP address, date, and time
+            string logFileName = $"Log_{DateTime.Now:dd-MMM-yyyy}.txt";
+            string logFilePath = Server.MapPath($"~/Logs/{logFileName}");
+            string publicIPAddress = GetUserIPAddress();
+            string logEntry = $"Recycle button executed at -- {DateTime.Now}{Environment.NewLine}IP Address: {publicIPAddress}{Environment.NewLine}";
+
+            // Check if log folder exists
+            string logFolder = Server.MapPath("~/Logs");
+            if (!Directory.Exists(logFolder))
+            {
+                //throw new DirectoryNotFoundException("Logs folder not found.");
+                message.InnerText = "Logs folder not found.";
+                message.Style["color"] = "red";
+            }
+
+            // Write log entry to file
+            File.AppendAllText(logFilePath, logEntry + Environment.NewLine);
+
+
+            //using (ServerManager serverManager = new ServerManager())
+            //{
+            //    ApplicationPool appPool = serverManager.ApplicationPools.FirstOrDefault(pool => pool.Name == selectedWebApp);
+
+            //    if (appPool != null)
+            //    {
+            //        try
+            //        {
+            //            appPool.Recycle();
+            //            message.InnerText = "Application pool " +selectedWebApp+ " has been recycled successfully.";
+            //            message.Style["color"] = "green";
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            message.InnerText = "Failed to recycle the application pool. Reason: " + ex.Message;
+            //            message.Style["color"] = "red";
+            //        }
+            //    }
+            //    else
+            //    {
+            //        message.InnerText = "Selected application pool not found.";
+            //        message.Style["color"] = "red";
+            //    }
+            //}
+
+
+            string selectedValue = webAppList.SelectedValue;
+
+            try
+            {
+                using (ServerManager serverManager = new ServerManager())
+                {
+                    if (IsCustomerName(selectedValue))
+                    {
+                        bool success = RecycleCustomerApplicationPools(serverManager, selectedValue);
+
+                        if (success)
+                        {
+                            message.InnerText = "Application pools for " + selectedValue + " have been recycled.";
+                            message.Style["color"] = "green";
+                        }
+                        else
+                        {
+                            message.InnerText = "Failed to recycle application pools for " + selectedValue + ".";
+                            message.Style["color"] = "red";
+                        }
+                    }
+                    else
+                    {
+                        bool success = RecycleIndividualApplicationPool(serverManager, selectedValue);
+
+                        if (success)
+                        {
+                            message.InnerText = "Application pool " + selectedValue + " has been recycled.";
+                            message.Style["color"] = "green";
+                        }
+                        else
+                        {
+                            message.InnerText = "Failed to recycle application pool " + selectedValue + ".";
+                            message.Style["color"] = "red";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                message.InnerText = "An error occurred: " + ex.Message;
+            }
+
+
+
+        }
+
+
+
+        private void LoadWebApps()
+        {
+            //string xmlFile = Server.MapPath("~/webApps.xml");
+            //XDocument doc = XDocument.Load(xmlFile);
+            //var webApps = doc.Descendants("Name");
+
+            //foreach (var webApp in webApps)
+            //{
+            //    string webAppName = webApp.Value;
+            //    if (!string.IsNullOrEmpty(webAppName))
+            //    {
+            //        webAppList.Items.Add(new ListItem(webAppName));
+            //    }
+            //}
+
+            string xmlFilePath = Server.MapPath("~/webApps.xml");
+
+            // Load the XML document
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlFilePath);
+
+            // Retrieve the customer data from the XML
+            XmlNodeList customerNodes = xmlDoc.SelectNodes("//Customer");
+
+            // Loop through each customer node and retrieve the customer name
+            foreach (XmlNode customerNode in customerNodes)
+            {
+                string customerName = customerNode.Attributes["Name"].Value;
+                XmlNodeList appPoolNodes = customerNode.SelectNodes("WebApplication");
+
+                // Add the customer name as an option in the dropdown
+                ListItem customerItem = new ListItem(customerName, customerName);
+                webAppList.Items.Add(customerItem);
+
+                // Loop through each application pool and add it to the dropdown
+                foreach (XmlNode appPoolNode in appPoolNodes)
+                {
+                    string appPoolName = appPoolNode.InnerText;
+
+                    // Add the individual application pool as an option in the dropdown
+                    ListItem appPoolItem = new ListItem(appPoolName, appPoolName);
+                    webAppList.Items.Add(appPoolItem);
+                }
+            }
+
+
+
+
+
+
+        }
+
+
+        private bool IsCustomerName(string value)
+        {
+            // Implement your logic to determine if the value represents a customer name
+            // For example, you can check if the value exists in the XML under the 'Name' attribute of 'Customer' nodes
+            // Return true if it represents a customer name, false otherwise
+            // Replace this placeholder implementation with your actual logic
+            return value.StartsWith("Customer");
+        }
+
+
+        private bool RecycleCustomerApplicationPools(ServerManager serverManager, string customerName)
+        {
+            // Retrieve the customer node from the XML
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Server.MapPath("~/webApps.xml"));
+            XmlNode customerNode = xmlDoc.SelectSingleNode("//Customer[@Name='" + customerName + "']");
+
+
+            if (customerNode != null)
+            {
+                // Retrieve the application pools for the selected customer
+                XmlNodeList appPoolNodes = customerNode.SelectNodes("WebApplication");
+
+                // Recycle each application pool for the customer
+                foreach (XmlNode appPoolNode in appPoolNodes)
+                {
+                    string appPoolName = appPoolNode.InnerText;
+
+                    // Recycle the application pool using the appPoolName
+                    bool success = RecycleApplicationPool(serverManager, appPoolName);
+
+                    if (!success)
+                    {
+                        // Error occurred while recycling the application pool
+                        return false;
+                    }
+                }
+
+                // All application pools recycled successfully
+                return true;
+            }
+
+            // Customer not found in the XML
+            return false;
+        }
+
+        private bool RecycleApplicationPool(ServerManager serverManager, string appPoolName)
+        {
+            try
+            {
+                // Recycle the individual application pool
+                ApplicationPool appPool = serverManager.ApplicationPools.FirstOrDefault(ap => ap.Name.Equals(appPoolName));
+
+                // Recycle the application pool if found
+                if (appPool != null)
+                {
+                    appPool.Recycle();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log or handle the exception as needed
+                // Display error message on the front end or log the error
+                message.InnerText = "Error occurred while recycling the application pool: " + ex.Message;
+            }
+
+            // Application pool not found or error occurred
+            return false;
+        }
+
+        private bool RecycleIndividualApplicationPool(ServerManager serverManager, string appPoolName)
+        {
+            try
+            {
+                // Recycle the individual application pool
+                ApplicationPool appPool = serverManager.ApplicationPools.FirstOrDefault(ap => ap.Name.Equals(appPoolName));
+
+                // Recycle the individual application pool if found
+                if (appPool != null)
+                {
+                    appPool.Recycle();
+                    return true;
+                }
+                else
+                {
+                    // Application pool not found
+                    message.InnerText = "Application pool " + appPoolName + " not found.";
+                    message.Style["color"] = "red";
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log or handle the exception as needed
+                // Display error message on the front end or log the error
+                message.InnerText = "Error occurred while recycling the application pool: " + ex.Message;
+            }
+
+            // Error occurred
+            return false;
+        }
+
+
+
+        private string GetUserIPAddress()
+        {
+            string userIPAddress = string.Empty;
+
+            string forwardedFor = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (!string.IsNullOrEmpty(forwardedFor))
+            {
+                string[] ipArray = forwardedFor.Split(',');
+                userIPAddress = ipArray[0].Trim();
+            }
+            else
+            {
+                userIPAddress = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+            }
+
+            return userIPAddress;
+        }
+
+
+
+
+
+    }
+}
